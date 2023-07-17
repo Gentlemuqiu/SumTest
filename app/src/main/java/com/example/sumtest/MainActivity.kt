bffd6f02542e4f1e030c90c09ef6c9641d987c56
Lixh <1627812101@qq.com>
2023-07-17 15:34:12 +0800

    "暂存"

--- app/src/main/java/com/example/sumtest/MainActivity.kt
+++ app/src/main/java/com/example/sumtest/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         ARouter.getInstance().build("/home/HomeActivity/").navigation()
-         finish()
+        finish()
     }
 }
---
+++ model_hot/src/main/java/com/example/model/hot/HotFragment.kt
@@ -0,0 +1,20 @@
+package com.example.model.hot
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.modle.hot.R
+
+class HotFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_hot, container, false)
+    }
+}
