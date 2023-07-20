25754b6755d8a83b487c4f4f57c6022ab0af094f
Lixh <1627812101@qq.com>
2023-07-20 16:17:16 +0800

    :bulb: "实现视频播放功能"

--- model_play/src/main/debug/ui/TestPlayActivity.kt
+++ model_play/src/main/debug/ui/TestPlayActivity.kt
@@ -2,30 +2,64 @@ package ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import com.example.model.play.model.Recommend
+import android.view.Window
+import android.view.WindowManager
+import com.example.modle.play.R
+
 import com.example.modle.play.databinding.ActivityPlayBinding
-import com.example.model.play.viewModel.RecommendViewModel
+import xyz.doikki.videocontroller.StandardVideoController
+import xyz.doikki.videocontroller.component.CompleteView
+import xyz.doikki.videocontroller.component.ErrorView
+import xyz.doikki.videocontroller.component.GestureView
+import xyz.doikki.videocontroller.component.VodControlView
+import xyz.doikki.videoplayer.player.VideoView
 
 class TestPlayActivity : AppCompatActivity() {
     private val mBinding: ActivityPlayBinding by lazy {
         ActivityPlayBinding.inflate(layoutInflater)
     }
-    private val recommendViewModel by lazy { ViewModelProvider(this)[RecommendViewModel::class.java] }
-    private lateinit var list: ArrayList<Recommend>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(mBinding.root)
-        recommendViewModel.getRecommendData()
-        list = ArrayList<Recommend>()
-        recommendViewModel.recommendData.observe(this, Observer {
-            data->
-            Log.d("nihao", "onCreate: ${data}")
-        })
-        mBinding.hello.setOnClickListener {
-            Log.d("hui", "onCreate: ${list}")
+        val window: Window = window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = resources.getColor(R.color.black) // 将R.color.black替换为你想要的颜色值
+        val BASE_URL =
+            "http://baobab.kaiyanapp.com/api/v1/playUrl?vid=223296&resourceType=video&editionType=default&source=aliyun&playUrlType=url_oss&udid="
+        val controller = StandardVideoController(this)
+        controller.addDefaultControlComponent("标题", false)
+        controller.setEnableOrientation(false)
+        controller.addControlComponent(CompleteView(this)) //自动完成播放界面
+        controller.addControlComponent(ErrorView(this)) //错误界面
+        controller.addControlComponent(GestureView(this))//滑动控制视图
+        controller.addControlComponent(VodControlView(this))
+        mBinding.player.setVideoController(controller) //设置控制器
+        mBinding.player.setScreenScaleType(VideoView.SCREEN_SCALE_16_9)
+        mBinding.player.setUrl(BASE_URL) //设置视频地址
+        mBinding.player.release()
+        mBinding.player.start() //开始播放，不调用则不自动播放
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mBinding.player.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mBinding.player.resume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mBinding.player.release()
+    }
+
+
+    override fun onBackPressed() {
+        if (!mBinding.player.onBackPressed()) {
+            super.onBackPressed()
         }
     }
 }
