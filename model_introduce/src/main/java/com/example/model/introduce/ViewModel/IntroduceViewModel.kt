e218405af86af9139a5b5418533f39d4f61e29b4
Lixh <1627812101@qq.com>
2023-07-25 20:51:59 +0800

    :bulb:"美化布局。"

--- model_introduce/src/main/java/com/example/model/introduce/ViewModel/IntroduceViewModel.kt
+++ model_introduce/src/main/java/com/example/model/introduce/ViewModel/IntroduceViewModel.kt
@@ -17,6 +17,8 @@ import kotlinx.coroutines.launch
 class IntroduceViewModel() : ViewModel() {
     private val _relatedCategoryData = MutableLiveData<RelatedCategory>()
     val relatedCategoryData: LiveData<RelatedCategory> get() = _relatedCategoryData
+
+    val category = MutableLiveData<List<RelatedCategory.Item>>()
     fun getRelatedCategoryData(id: Int) {
         viewModelScope.launch {
             flow {
@@ -29,6 +31,27 @@ class IntroduceViewModel() : ViewModel() {
                 }
                 .collect {
                     _relatedCategoryData.value = it
+                    category.value=it.itemList
+                }
+        }
+    }
+
+    private val _newCateGoryData = MutableLiveData<RelatedCategory>()
+    val newCateGoryData: LiveData<RelatedCategory> get() = _newCateGoryData
+    fun getNewCateGory(url: String) {
+        viewModelScope.launch {
+            flow {
+                val list = NetIntroduce.getNewCateGory(url)
+                emit(list)
+            }.flowOn(Dispatchers.IO)
+                .catch { e ->
+                    e.printStackTrace()
+                    Log.d("hui", "getCateGory: ${e}")
+                }
+                .collect {
+                    _newCateGoryData.value = it
+                    val oldData = category.value ?: emptyList()
+                    category.value=oldData+it.itemList
                 }
         }
     }
--- modle_playground/src/main/java/com/example/modle_playground/MyImage.kt
+++ modle_playground/src/main/java/com/example/modle_playground/MyImage.kt
@@ -9,7 +9,6 @@ object MyImage {
         R.drawable.number4, R.drawable.number5, R.drawable.number6,
         R.drawable.number7, R.drawable.number8, R.drawable.number9,
         R.drawable.number10, R.drawable.number11, R.drawable.number12,
-        R.drawable.number13, R.drawable.number14, R.drawable.number15,
-        R.drawable.number16
+        R.drawable.number13,
     )
 }
